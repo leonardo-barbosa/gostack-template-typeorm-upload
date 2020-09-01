abd5d73e1bd81f2a918cc93a477c8250772577bd
Leonardo Moura <leonardo.moura.barbosa@outlook.com>
2020-08-31 21:19:13 -0300

    feat: add transaction deletion

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const checkTransactionExists = await transactionsRepository.findOne(id);
+
+    if (!checkTransactionExists) {
+      throw new AppError('No trasaction found with given id.');
+    }
+
+    await transactionsRepository.remove(checkTransactionExists);
   }
 }
 
